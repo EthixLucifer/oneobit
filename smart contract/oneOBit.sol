02a36f3668391a52cbaf10380b3ba7b94b3a93a1
EthixLucifer <luciferethix@gmail.com>
2022-11-02 14:08:16 +0530

    updated contract

--- smart contract/oneOBit.sol
+++ smart contract/oneOBit.sol
@@ -1,31 +1,42 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.6;
-import "erc721a/contracts/ERC721A.sol";
-import "contracts/.deps/github/OpenZeppelin/openzeppelin-contracts/contracts/access/Ownable.sol";
 
-contract OneOBitAssignment is ERC721A, Ownable {
-    constructor() ERC721A("OneOBit", "OOB") {}
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
-    // uint256 public mintingFees = 0.001 ether;
-    string baseURI = "https://ipfs.io/ipfs/";
+contract mintNft is ERC721URIStorage, ReentrancyGuard {
+    string public baseURI = "https://Address/";
 
-    function claim(address to, uint256 quantity) public {
-        require(quantity >= 0, "Minimum minting must be greater than zero");
-        // require(msg.value == (quantity*mintingFees), "Not enough minting fees sent");
-        _safeMint(to, quantity);
+    constructor() ERC721("OneOBit", "OOB") {}
+
+    event claimed(uint256 indexed tokenId);
+
+    function claim(uint256 tokenId)
+        public
+        nonReentrant
+        returns (uint256 _tokenId)
+    {
+        require(!_exists(tokenId), "This NFT has already been Minted");
+
+        _safeMint(msg.sender, tokenId);
+        string memory tokenURI = string(
+            bytes.concat(
+                bytes(_baseURI()),
+                "/",
+                bytes(Strings.toString(tokenId))
+            )
+        );
+        _setTokenURI(tokenId, tokenURI);
+
+        emit claimed(tokenId);
+        return tokenId;
     }
 
-    function _baseURI() internal view override returns (string memory URI) {
+    function _baseURI() internal view override returns (string memory) {
         return baseURI;
-    }
-
-    function _startTokenId() internal pure override returns (uint256) {
-        return (1);
-    }
-
-    function tokensMintedByUser() public view returns (uint256) {
-        return (_numberMinted(msg.sender));
     }
 }
 
-// goer Addr : "0x4Af1fA82FFCfA99940cf203A7d4e1f022B61661E";
+// goer Addr : "0xeE47F4743f5ae3E23bdDDBa934db5B01661fE368";
